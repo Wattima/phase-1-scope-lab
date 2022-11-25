68bb964f21416a69b8ecb240ebe64031a25a35a1
Daryl Wattima <darwattima@gmail.com>
2022-11-25 16:55:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+var customerName = 'bob';
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+} 
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'; 
+}
+
+const leastFavoriteCustomer = 'Lucy';
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'John';
+}
